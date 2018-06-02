6d61efc7ef5921be6a6125958568bf87554914d7
Your Name <alsouzara@gmail.com>
2018-06-02 03:29:19 -0300

    New with mutate

--- dplyr.R
+++ dplyr.R
@@ -1,10 +1,11 @@
 #PACOTE
-if(require(dplyr)==F) install.packages("dplyr")
+if(!require(dplyr)) install.packages("dplyr")
 
 #DADOS
 dados=read.csv("http://mwstat.com/andersonara/IVD/dados1.csv",
                sep=";",stringsAsFactors = T)
-names(dados)
+
+glimpse(dados)
 
 #FUNÇÃO SELECT
 select(dados,Q1)
@@ -15,6 +16,9 @@ select(dados,-Q3)
 select(dados,-Q3,-Q5)
 select(dados,-c(Q3,Q5))
 select(dados,-c(3,5))
+select(dados, starts_with("Q"))
+select(dados, ends_with("9"))
+select(dados, contains("1"))
 
 #FUNÇÃO RENAME
 rename(dados,quest1=Q1,quest2=Q2)
@@ -22,3 +26,28 @@ rename(dados,quest1=Q1,quest2=Q2)
 #UTILIZANDO O COMANDO PIPE %>%
 dados %>% select(Q1)
 dados %>% rename(quest1=Q1)
+
+#FUNÇÃO FILTER
+
+filter(dados,Q5=="S",Q1>20)
+filter(dados,Q5=="S",Q1>20,Q9==6)
+filter(dados,Q5=="S",Q1>20 & Q9==6)
+filter(dados,Q5=="S",Q1>20 | Q9==6)
+
+dados %>% filter (Q1>20,Q9>=6)
+
+#FUNÇÃO SLICE
+slice(dados,1:10)
+dados %>% slice (1:10)
+
+#FUNÇÃO MUTATE
+mutate(dados,Q10=Q6+Q9)
+
+dados %>% mutate(Idade_c=if_else(Q1>mean(Q1),"Acima","Abaixo"))
+
+#FUNÇÃO TRANSMUTE
+transmute(dados,Q1,Q10=Q6+Q9)
+
+dados %>% transmute(Q1,Q10=Q6+Q9)
+dados %>% transmute(Idade_c=if_else(Q1>mean(Q1),"Acima","Abaixo"))
+
